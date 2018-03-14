ed96a51921cff434837bc55f146a04f7e4563711
Sanketh M. S <36821143+sms006@users.noreply.github.com>
2018-03-14 18:43:21 +0100

    Update look.js
    route to accept get method containing user search parameters.

--- routes/look.js
+++ routes/look.js
@@ -1,9 +1,16 @@
 var express = require('express');
-var router = express.Router();
+var getloc = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+//captures the radius, longitude and latitude from the get request in order to locate nearby locations within the radius
+var radi, lati, lngi;
+getloc.get('/submitloc', function (req,res) {
+
+    radi = req.query.radius;
+    lati = req.query.lat;
+    lngi = req.query.lng;
+
+    console.log(radi,lati,lngi);
+
 });
 
-module.exports = router;
+module.exports = {router:getloc, rad:radi, lat:lati, lng:lngi};
