f9e49bb2b9fe2e57e61ce55b49e92b8bc5e0074d
Sanketh M. S <36821143+sms006@users.noreply.github.com>
2018-03-15 19:40:30 +0100

    calling select.js resolved
    This versions correctly calls and passes vars to select.js

--- routes/look.js
+++ routes/look.js
@@ -1,16 +1,18 @@
 var express = require('express');
 var getloc = express.Router();
+var getopps = require('../model/select');
 
 //captures the radius, longitude and latitude from the get request in order to locate nearby locations within the radius
-var radi, lati, lngi;
-getloc.get('/submitloc', function (req,res) {
+getloc.get('/submitloc', function (req, res) {
 
-    radi = req.query.radius;
-    lati = req.query.lat;
-    lngi = req.query.lng;
+    var radi = req.query.radius;
+    var lati = req.query.lat;
+    var lngi = req.query.lng;
 
-    console.log(radi,lati,lngi);
-
+    console.log(radi, lngi, lati);
+    getopps.findopps(radi, lati, lngi);
+    res.send('check console for results');
 });
 
-module.exports = {router:getloc, rad:radi, lat:lati, lng:lngi};
+module.exports = getloc;
+
