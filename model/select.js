21a8d81c423eff0bdac8d7766c89b2ef92831f2b
Sanketh M. S <36821143+sms006@users.noreply.github.com>
2018-03-15 19:38:58 +0100

    Exporting and passing vars resolved
    This version integrates better with the other files unlike the last one which did not export and catch parameters the right way

--- model/select.js
+++ model/select.js
@@ -1,17 +1,36 @@
 var express = require('express');
-var router = express.Router();
+var look = require('../routes/look');
 var mysql = require('mysql');
 
-var look = require('../routes/look');
-var connect = require('../model/connect');
-
-var sql = 'SELECT id, name, address, lat, lng, ( 3959 * acos( cos( radians('+ look.lat + ') ) * cos( radians( lat ) )' +
-    ' * cos( radians( lng ) - radians('+look.lng+') ) + sin( radians('+look.lat+') ) * sin( radians( lat ) ) ) ) ' +
-    'AS distance FROM markers HAVING distance < '+look.rad+' ORDER BY distance LIMIT 0 , 20';
-
-var query = connect.connection.query(sql, function (err, results) {
-
-    if (err) throw err;
-    console.log(results);
-
+// connect to the MySQL db on localhost
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : '',
+    database : 'sad_test1'
 });
+
+
+module.exports = {
+    //This function catches the search parameters given by the user namely the radius, latitude and longitude
+    findopps: function (rad,lat,lng) {
+
+        //This sql query looks for entries in the table which are within the radius by using a haversine formula
+       var sql = 'SELECT id, name, address, lat, lng, ( 3959 * acos( cos( radians('+lat+') ) * cos( radians( lat ) )' +
+          ' * cos( radians( lng ) - radians('+lng+') ) + sin( radians('+lat+') ) * sin( radians( lat ) ) ) )' +
+        ' AS distance FROM markers HAVING distance < ' + rad + ' ORDER BY distance LIMIT 0 , 20';
+
+        //connecting to the db
+        connection.connect(function(err) {
+            if (err) throw err;
+
+            //executing the sql query
+            connection.query(sql, function (err, results) {
+                if (err) throw err;
+
+                console.log(results);
+
+            });
+        });
+    }
+};
