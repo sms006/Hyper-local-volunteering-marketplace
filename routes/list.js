52b3580537d5038ecb5a44feaef7e7a469c4b880
Sanketh M. S <36821143+sms006@users.noreply.github.com>
2018-03-21 17:26:07 +0100

    Get parameter name updated

--- routes/list.js
+++ routes/list.js
@@ -4,15 +4,13 @@ var putopps = require('../model/insert');
 
 getemp.get('/setopportunity', function (req, res) {
 
-    var name = req.query.radius;
-    var email = req.query.lat;
-    var workd = req.query.lng;
-    var address = req.query.radius;
+    var name = req.query.name;
+    var email = req.query.email;
+    var workd = req.query.task;
+    var address = req.query.address;
     var lati = req.query.lat;
     var lngi = req.query.lng;
-    var typeofwork = req.query.tow;
-
-    console.log();
+    var typeofwork = req.query.choose;
     putopps.putopps(name,email,workd,address,lati,lngi,typeofwork);
 
 });
