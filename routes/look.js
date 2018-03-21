191c718f34fa6197852e9e51b842e2aa63e6cbfa
Sanketh M. S <36821143+sms006@users.noreply.github.com>
2018-03-21 22:04:27 +0100

    Result string caught in callback

--- routes/look.js
+++ routes/look.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var getloc = express.Router();
 var getopps = require('../model/select');
+var reo = require('../views/resptemplate');
 
 //captures the radius, longitude and latitude from the get request in order to locate nearby locations within the radius
 getloc.get('/submitloc', function (req, res) {
@@ -8,11 +9,13 @@ getloc.get('/submitloc', function (req, res) {
     var radi = req.query.radius;
     var lati = req.query.lat;
     var lngi = req.query.lng;
-
-    console.log(radi, lngi, lati);
-    getopps.findopps(radi, lati, lngi);
-    res.send('check console for results');
+    var tow =  req.query.choose;
+    var final = '';
+    getopps.findopps(radi, lati, lngi,tow, function getresult(tab){
+        final = reo.reo.replace('{${table}}', tab);
+        res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+        res.write(final, 'utf-8');
+        res.end();
+    });
+    //console.log(getopps.sendhtml());
 });
-
-module.exports = getloc;
-
