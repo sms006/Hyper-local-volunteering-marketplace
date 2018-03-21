4f1af637c7d64d5ea1db75793bda9fc480248e10
sampathkumarakash <36840306+sampathkumarakash@users.noreply.github.com>
2018-03-21 11:12:18 +0100

    Add files via upload
    table with header and footer

--- viewkdkd.js
+++ viewkdkd.js
@@ -10,7 +10,7 @@ var methods = {
             host: 'localhost',
             user: 'root',
             password: '',
-            database: 'voluntary'
+            database: 'sad_test1'
         });
 
 
@@ -32,7 +32,7 @@ var methods = {
 
                     //create html table with data from res.
                     for (var i = 0; i < res.length; i++) {
-                        table += reo.otrotd + (i + 1) + reo.ctdotd + res[i].purpose + reo.ctdotd + res[i].pin + reo.ctdotd + res[i].time + reo.ctdctr ;
+                        table += reo.otrotd + (i + 1) + reo.ctdotd + res[i].workid + reo.ctdotd + res[i].taskid + reo.ctdotd + res[i].employerid + reo.ctdotd + res[i].work_description + reo.ctdotd + res[i].address + reo.ctdotd + res[i].lat + reo.ctdotd + res[i].lng + reo.ctdctr ;
                     }
                     table = reo.tablehead + table + reo.tableend ;
 
@@ -43,7 +43,7 @@ var methods = {
             });
         };
 
-            let sql = 'SELECT purpose,pin,time FROM request';
+            let sql = 'SELECT * FROM marketplace';
             const server = http.createServer((req, res) => {
                 setResHtml(sql, resql => {
                     file = file.replace('{${table}}', resql);
@@ -59,6 +59,25 @@ var methods = {
             });
         }
 };
+// var file = meg.meg;
+// var publicConfig = {
+//     key: 'AIzaSyA1gnpTMv_3WFP4BzJ28vBLZTbaCkahw7E',
+//     stagger_time:       1000, // for elevationPath
+//     encode_polylines:   false,
+//     secure:             true, // use https
+//     proxy:              'http://127.0.0.1:9999' // optional, set a proxy for HTTP requests
+// };
+// var GoogleMapsAPI = require('googlemaps');
+//
+// var gmAPI = new GoogleMapsAPI(publicConfig);
+// var reverseGeocodeParams = {
+//     "latlng":        "51.1245,-0.0523",
+//     "result_type":   "postal_code",
+//     "language":      "en",
+//     "location_type": "APPROXIMATE"
+// };
+//
+// gmAPI.reverseGeocode(reverseGeocodeParams, function(err, result){
+//     console.log(result);
+// });
 exports.data = methods;
-
-
