ee63ebfe55cb8a7a47772d09ead4cd3a1f557a40
Sanketh M. S <36821143+sms006@users.noreply.github.com>
2018-03-25 13:38:10 +0200

    Static resources now rendered

--- routes/pages.js
+++ routes/pages.js
@@ -3,21 +3,20 @@ var sendpages = express.Router();
 var vol = require('../volunteer');
 var path = require('path');
 
-sendpages.use('/',express.static(path.join('S:/Course Mets/sadproj/public', 'stylesheets')));
+sendpages.use(express.static('public'));
 
 sendpages.get('/', function(req, res, next) {
-    console.log(__dirname);
     res.sendFile('./index.html',{root: path.join('S:/Course Mets/sadproj','views')});
 });
 
 sendpages.get('/list.html', function(req, res, next) {
     res.sendFile('./list.html',{root: path.join('S:/Course Mets/sadproj','views')});
-    res.end();
+
 });
 
 sendpages.get('/look.html', function(req, res, next) {
     res.sendFile('./look.html',{root: path.join('S:/Course Mets/sadproj','views')});
-    res.end();
+
 });
 
 module.exports = sendpages;
